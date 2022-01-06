467d06403e6889b7056b5210184c9d63daec5784
Tiziano <tizianocitro98@gmail.com>
2022-01-06 16:53:17 +0100

    Testing improvements

--- src/test/java/it/unisa/studenti/citro/tiziano/SudokuGameTest.java
+++ src/test/java/it/unisa/studenti/citro/tiziano/SudokuGameTest.java
@@ -17,6 +17,7 @@ import java.util.List;
 import static it.unisa.studenti.citro.tiziano.model.PlaceNumber.getPlaceNumber;
 import static it.unisa.studenti.citro.tiziano.model.PlaceOptions.*;
 import static it.unisa.studenti.citro.tiziano.sudoku.model.Grid.FIRST;
+import static it.unisa.studenti.citro.tiziano.sudoku.model.Grid.TOTAL_BLOCK_NUMBER;
 import static it.unisa.studenti.citro.tiziano.sudoku.utils.Scores.*;
 import static it.unisa.studenti.citro.tiziano.sudoku.utils.SudokuUtils.ERROR_WHILE_PLACING_NUMBER;
 import static org.junit.jupiter.api.Assertions.*;
@@ -49,6 +50,12 @@ public class SudokuGameTest {
     public void generateAlreadyExistingSudoku() {
         assertNull(peer1.generateNewSudoku(GAME),
                 "PEER-ONE has tried to generate GAME game again");
+        assertNull(peer2.generateNewSudoku(GAME),
+                "PEER-TWO has tried to generate GAME game again");
+        assertNull(peer3.generateNewSudoku(GAME),
+                "PEER-THREE has tried to generate GAME game again");
+        assertNull(peer4.generateNewSudoku(GAME),
+                "PEER-FOUR has tried to generate GAME game again");
     }
 
     /**
@@ -57,8 +64,14 @@ public class SudokuGameTest {
     @Test
     @Order(3)
     public void joinNotExistingGame() {
-        assertFalse(peer2.join(NOT_EXISTING_GAME, PEER_ONE),
+        assertFalse(peer1.join(NOT_EXISTING_GAME, PEER_ONE),
+                "PEER-ONE has tried to join a not existing game");
+        assertFalse(peer2.join(NOT_EXISTING_GAME, PEER_TWO),
                 "PEER-TWO has tried to join a not existing game");
+        assertFalse(peer3.join(NOT_EXISTING_GAME, PEER_THREE),
+                "PEER-THREE has tried to join a not existing game");
+        assertFalse(peer4.join(NOT_EXISTING_GAME, PEER_FOUR),
+                "PEER-FOUR has tried to join a not existing game");
     }
 
     /**
@@ -67,8 +80,17 @@ public class SudokuGameTest {
     @Test
     @Order(4)
     public void joinAlreadyJoinedGame() {
-        assertTrue(peer2.join(GAME, PEER_ONE), "PEER-TWO has joined the game GAME");
-        assertFalse(peer2.join(GAME, PEER_ONE), "PEER-TWO has tried to join the game GAME again");
+        assertTrue(peer1.join(GAME, PEER_ONE), "PEER-ONE has joined the game GAME");
+        assertFalse(peer1.join(GAME, PEER_ONE), "PEER-ONE has tried to join the game GAME again");
+
+        assertTrue(peer2.join(GAME, PEER_TWO), "PEER-TWO has joined the game GAME");
+        assertFalse(peer2.join(GAME, PEER_TWO), "PEER-TWO has tried to join the game GAME again");
+
+        assertTrue(peer3.join(GAME, PEER_THREE), "PEER-THREE has joined the game GAME");
+        assertFalse(peer3.join(GAME, PEER_THREE), "PEER-THREE has tried to join the game GAME again");
+
+        assertTrue(peer4.join(GAME, PEER_FOUR), "PEER-FOUR has joined the game GAME");
+        assertFalse(peer4.join(GAME, PEER_FOUR), "PEER-FOUR has tried to join the game GAME again");
     }
 
     /**
@@ -123,6 +145,15 @@ public class SudokuGameTest {
     public void leaveGame() {
         assertTrue(peer1.join(GAME, PEER_ONE), "PEER-ONE has joined the game GAME");
         assertTrue(peer1.leave(GAME), "PEER-ONE has left the game GAME");
+
+        assertTrue(peer2.join(GAME, PEER_TWO), "PEER-TWO has joined the game GAME");
+        assertTrue(peer2.leave(GAME), "PEER-TWO has left the game GAME");
+
+        assertTrue(peer3.join(GAME, PEER_THREE), "PEER-THREE has joined the game GAME");
+        assertTrue(peer3.leave(GAME), "PEER-THREE has left the game GAME");
+
+        assertTrue(peer4.join(GAME, PEER_FOUR), "PEER-FOUR has joined the game GAME");
+        assertTrue(peer4.leave(GAME), "PEER-FOUR has left the game GAME");
     }
 
     /**
@@ -132,6 +163,9 @@ public class SudokuGameTest {
     @Order(10)
     public void leaveNotJoinedGame() {
         assertFalse(peer1.leave(GAME), "PEER-ONE has not joined the game GAME, thus cannot left");
+        assertFalse(peer2.leave(GAME), "PEER-TWO has not joined the game GAME, thus cannot left");
+        assertFalse(peer3.leave(GAME), "PEER-THREE has not joined the game GAME, thus cannot left");
+        assertFalse(peer4.leave(GAME), "PEER-FOUR has not joined the game GAME, thus cannot left");
     }
 
     /**
@@ -141,6 +175,9 @@ public class SudokuGameTest {
     @Order(11)
     public void leaveNotExistingGame() {
         assertFalse(peer1.leave(NOT_EXISTING_GAME), "PEER-ONE has tried to leave a not existing game");
+        assertFalse(peer2.leave(NOT_EXISTING_GAME), "PEER-TWO has tried to leave a not existing game");
+        assertFalse(peer3.leave(NOT_EXISTING_GAME), "PEER-THREE has tried to leave a not existing game");
+        assertFalse(peer4.leave(NOT_EXISTING_GAME), "PEER-FOUR has tried to leave a not existing game");
     }
 
     /**
@@ -246,8 +283,14 @@ public class SudokuGameTest {
     @Test
     @Order(16)
     public void placeNumberInNotJoinedGame() {
+        assertEquals(ERROR_WHILE_PLACING_NUMBER, peer1.placeNumber(GAME, FIRST, FIRST, FIRST),
+                "PEER-ONE has tried to place a number in a game that has not joined yet");
+        assertEquals(ERROR_WHILE_PLACING_NUMBER, peer2.placeNumber(GAME, FIRST, FIRST, FIRST),
+                "PEER-TWO has tried to place a number in a game that has not joined yet");
         assertEquals(ERROR_WHILE_PLACING_NUMBER, peer3.placeNumber(GAME, FIRST, FIRST, FIRST),
                 "PEER-THREE has tried to place a number in a game that has not joined yet");
+        assertEquals(ERROR_WHILE_PLACING_NUMBER, peer4.placeNumber(GAME, FIRST, FIRST, FIRST),
+                "PEER-FOUR has tried to place a number in a game that has not joined yet");
     }
 
     /**
@@ -256,8 +299,158 @@ public class SudokuGameTest {
     @Test
     @Order(17)
     public void placeNumberInNotExistingGame() {
+        assertEquals(ERROR_WHILE_PLACING_NUMBER, peer1.placeNumber(NOT_EXISTING_GAME, FIRST, FIRST, FIRST),
+                "PEER-ONE has tried to place a number in a game that does not exist");
+        assertEquals(ERROR_WHILE_PLACING_NUMBER, peer2.placeNumber(NOT_EXISTING_GAME, FIRST, FIRST, FIRST),
+                "PEER-TWO has tried to place a number in a game that does not exist");
+        assertEquals(ERROR_WHILE_PLACING_NUMBER, peer3.placeNumber(NOT_EXISTING_GAME, FIRST, FIRST, FIRST),
+                "PEER-THREE has tried to place a number in a game that does not exist");
         assertEquals(ERROR_WHILE_PLACING_NUMBER, peer4.placeNumber(NOT_EXISTING_GAME, FIRST, FIRST, FIRST),
                 "PEER-FOUR has tried to place a number in a game that does not exist");
+    }
+
+    /**
+     * Tests that a peer cannot create a new game with empty name.
+     */
+    @Test
+    @Order(18)
+    public void generateNewSudokuWithEmptyName() {
+        assertNull(peer1.generateNewSudoku(""), "PEER-ONE has tried to generate a game with empty name");
+    }
+
+    /**
+     * Tests that peers cannot join when providing empty name for a game.
+     */
+    @Test
+    @Order(19)
+    public void joinSudokuWithEmptyName() {
+        assertFalse(peer1.join("", PEER_ONE), "PEER-ONE has tried to join a game with empty name");
+        assertFalse(peer2.join("", PEER_TWO), "PEER-TWO has tried to join a game with empty name");
+        assertFalse(peer3.join("", PEER_THREE), "PEER-THREE has tried to join a game with empty name");
+        assertFalse(peer4.join("", PEER_FOUR), "PEER-FOUR has tried to join a game with empty name");
+    }
+
+    /**
+     * Tests that peers cannot join a game without providing a nickname.
+     */
+    @Test
+    @Order(20)
+    public void joinSudokuWithoutNickname() {
+        assertFalse(peer1.join(GAME, ""), "PEER-ONE has tried to join a game without providing a nickname");
+        assertFalse(peer2.join(GAME, ""), "PEER-TWO has tried to join a game without providing a nickname");
+        assertFalse(peer3.join(GAME, ""), "PEER-THREE has tried to join a game without providing a nickname");
+        assertFalse(peer4.join(GAME, ""), "PEER-FOUR has tried to join a game without providing a nickname");
+    }
+
+    /**
+     * Tests the possibility to retrieve a joined game grid by providing an empty name for the game.
+     */
+    @Test
+    @Order(21)
+    public void getSudokuWhitEmptyName() {
+        assertTrue(peer4.join(GAME, PEER_FOUR), "PEER-FOUR has joined the game GAME");
+        Integer[][] sudoku = peer4.getSudoku("");
+        assertNull(sudoku, "PEER-FOUR has tried to get a grid for a name but it has given an empty name");
+    }
+
+    /**
+     * Tests that a peer cannot place a number when it has specified a wrong row index,
+     * lower than the minimum possible.
+     */
+    @Test
+    @Order(22)
+    public void placeNumberInRowLowerThanMin() {
+        assertEquals(ERROR_WHILE_PLACING_NUMBER, peer1.placeNumber(GAME, FIRST - 1, FIRST, FIRST),
+                "PEER-ONE has tried to place a number in a not valid row");
+        assertEquals(ERROR_WHILE_PLACING_NUMBER, peer2.placeNumber(GAME, FIRST - 1, FIRST, FIRST),
+                "PEER-TWO has tried to place a number in a not valid row");
+        assertEquals(ERROR_WHILE_PLACING_NUMBER, peer3.placeNumber(GAME, FIRST - 1, FIRST, FIRST),
+                "PEER-THREE has tried to place a number in a not valid row");
+        assertEquals(ERROR_WHILE_PLACING_NUMBER, peer4.placeNumber(GAME, FIRST - 1, FIRST, FIRST),
+                "PEER-FOUR has tried to place a number in a not valid row");
+    }
+
+    /**
+     * Tests that a peer cannot place a number when it has specified a wrong row index,
+     * higher than the maximum possible.
+     */
+    @Test
+    @Order(23)
+    public void placeNumberInRowHigherThanMax() {
+        assertEquals(ERROR_WHILE_PLACING_NUMBER, peer1.placeNumber(GAME, TOTAL_BLOCK_NUMBER + 1, FIRST, FIRST),
+                "PEER-ONE has tried to place a number in a not valid row");
+        assertEquals(ERROR_WHILE_PLACING_NUMBER, peer2.placeNumber(GAME, TOTAL_BLOCK_NUMBER + 1, FIRST, FIRST),
+                "PEER-TWO has tried to place a number in a not valid row");
+        assertEquals(ERROR_WHILE_PLACING_NUMBER, peer3.placeNumber(GAME, TOTAL_BLOCK_NUMBER + 1, FIRST, FIRST),
+                "PEER-THREE has tried to place a number in a not valid row");
+        assertEquals(ERROR_WHILE_PLACING_NUMBER, peer4.placeNumber(GAME, TOTAL_BLOCK_NUMBER + 1, FIRST, FIRST),
+                "PEER-FOUR has tried to place a number in a not valid row");
+    }
+
+    /**
+     * Tests that a peer cannot place a number when it has specified a wrong column index,
+     * lower than the minimum possible.
+     */
+    @Test
+    @Order(24)
+    public void placeNumberInColumnLowerThanMin() {
+        assertEquals(ERROR_WHILE_PLACING_NUMBER, peer1.placeNumber(GAME, FIRST, FIRST - 1, FIRST),
+                "PEER-ONE has tried to place a number in a not valid column");
+        assertEquals(ERROR_WHILE_PLACING_NUMBER, peer2.placeNumber(GAME, FIRST, FIRST - 1, FIRST),
+                "PEER-TWO has tried to place a number in a not valid column");
+        assertEquals(ERROR_WHILE_PLACING_NUMBER, peer3.placeNumber(GAME, FIRST, FIRST - 1, FIRST),
+                "PEER-THREE has tried to place a number in a not valid column");
+        assertEquals(ERROR_WHILE_PLACING_NUMBER, peer4.placeNumber(GAME, FIRST, FIRST - 1, FIRST),
+                "PEER-FOUR has tried to place a number in a not valid column");
+    }
+
+    /**
+     * Tests that a peer cannot place a number when it has specified a wrong column index,
+     * higher than the maximum possible.
+     */
+    @Test
+    @Order(25)
+    public void placeNumberInColumnHigherThanMax() {
+        assertEquals(ERROR_WHILE_PLACING_NUMBER, peer1.placeNumber(GAME, FIRST, TOTAL_BLOCK_NUMBER + 1, FIRST),
+                "PEER-ONE has tried to place a number in a not valid column");
+        assertEquals(ERROR_WHILE_PLACING_NUMBER, peer2.placeNumber(GAME, FIRST, TOTAL_BLOCK_NUMBER + 1, FIRST),
+                "PEER-TWO has tried to place a number in a not valid column");
+        assertEquals(ERROR_WHILE_PLACING_NUMBER, peer3.placeNumber(GAME, FIRST, TOTAL_BLOCK_NUMBER + 1, FIRST),
+                "PEER-THREE has tried to place a number in a not valid column");
+        assertEquals(ERROR_WHILE_PLACING_NUMBER, peer4.placeNumber(GAME, FIRST, TOTAL_BLOCK_NUMBER + 1, FIRST),
+                "PEER-FOUR has tried to place a number in a not valid column");
+    }
+
+    /**
+     * Tests that a peer cannot place a number when it ha specified a number lower than the minimum possible.
+     */
+    @Test
+    @Order(26)
+    public void placeNumberLowerThanMin() {
+        assertEquals(ERROR_WHILE_PLACING_NUMBER, peer1.placeNumber(GAME, FIRST, FIRST, FIRST - 1),
+                "PEER-ONE has tried to place a number lower than the minimum possible");
+        assertEquals(ERROR_WHILE_PLACING_NUMBER, peer2.placeNumber(GAME, FIRST, FIRST, FIRST - 1),
+                "PEER-TWO has tried to place a number lower than the minimum possible");
+        assertEquals(ERROR_WHILE_PLACING_NUMBER, peer3.placeNumber(GAME, FIRST, FIRST, FIRST - 1),
+                "PEER-THREE has tried to place a number lower than the minimum possible");
+        assertEquals(ERROR_WHILE_PLACING_NUMBER, peer4.placeNumber(GAME, FIRST, FIRST, FIRST - 1),
+                "PEER-FOUR has tried to place a number lower than the minimum possible");
+    }
+
+    /**
+     * Tests that a peer cannot place a number when it ha specified a number higher than the maximum possible.
+     */
+    @Test
+    @Order(27)
+    public void placeNumberHigherThanMax() {
+        assertEquals(ERROR_WHILE_PLACING_NUMBER, peer1.placeNumber(GAME, FIRST, FIRST, TOTAL_BLOCK_NUMBER + 1),
+                "PEER-ONE has tried to place a number higher than the maximum possible");
+        assertEquals(ERROR_WHILE_PLACING_NUMBER, peer2.placeNumber(GAME, FIRST, FIRST, TOTAL_BLOCK_NUMBER + 1),
+                "PEER-TWO has tried to place a number higher than the maximum possible");
+        assertEquals(ERROR_WHILE_PLACING_NUMBER, peer3.placeNumber(GAME, FIRST, FIRST, TOTAL_BLOCK_NUMBER + 1),
+                "PEER-THREE has tried to place a number higher than the maximum possible");
+        assertEquals(ERROR_WHILE_PLACING_NUMBER, peer4.placeNumber(GAME, FIRST, FIRST, TOTAL_BLOCK_NUMBER + 1),
+                "PEER-FOUR has tried to place a number higher than the maximum possible");
     }
 
     /**
